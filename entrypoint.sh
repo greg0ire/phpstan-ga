b00e87bde22932df0e56427dac4ba0c6dfb105a2
Grégoire Paris <postmaster@greg0ire.fr>
2020-03-26 18:45:13 +0100

    Wrap composer install in a group

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ if [ "$REQUIRE_DEV" = "true" ]; then
     NO_DEV=""
 fi
 
+echo "::group::composer install"
 composer install --no-scripts --no-progress --quiet $NO_DEV
+echo "::endgroup"
 /composer/vendor/bin/phpstan $*
